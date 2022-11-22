9f57e76c2a4b746360106f8ce7004f92aeb6de42
stormytuna <stormytuna@outlook.com>
2022-11-22 11:09:56 +0000

    Fix armor prefixes being available when they shouldnt

--- Common/Systems/DetourSystem.cs
+++ Common/Systems/DetourSystem.cs
@@ -1,14 +1,19 @@
-﻿using Terraria;
+﻿using ArmorAndAccessoryPrefixes.Common.Config;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace ArmorAndAccessoryPrefixes.Common.Systems {
     public class DetourSystem : ModSystem {
         public override void Load() {
-            On.Terraria.UI.ItemSlot.MouseHover_ItemArray_int_int += ItemSlot_MouseHover_ItemArray_int_int;
+            if (ServerConfig.Instance.ArmorPrefixes) {
+                On.Terraria.UI.ItemSlot.MouseHover_ItemArray_int_int += ItemSlot_MouseHover_ItemArray_int_int;
+            }
         }
 
         public override void Unload() {
-            On.Terraria.UI.ItemSlot.MouseHover_ItemArray_int_int -= ItemSlot_MouseHover_ItemArray_int_int;
+            if (ServerConfig.Instance.ArmorPrefixes) {
+                On.Terraria.UI.ItemSlot.MouseHover_ItemArray_int_int -= ItemSlot_MouseHover_ItemArray_int_int;
+            }
         }
 
         private void ItemSlot_MouseHover_ItemArray_int_int(On.Terraria.UI.ItemSlot.orig_MouseHover_ItemArray_int_int orig, Terraria.Item[] inv, int context, int slot) {
